64b8cf0c6ea7c3cfb45c1426d3cf356f8aeacba3
chech2 <90683516+chech2@users.noreply.github.com>
2023-01-23 20:18:14 +0900

    [Bronze I] Title: 더하기 사이클, Time: 124 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -1,21 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-	public static void main(String[] args) {
+	public int cal(int a) {
+		int tens = a / 10;
+		int units = a % 10;
+		return units * 10 + (tens + units) % 10;
+		
+		
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader bs = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N  = Integer.parseInt(bs.readLine());
+		Main my = new Main();
+		int cnt = 1;
+		int NN = my.cal(N);
+		while(N != NN) {
+			NN = my.cal(NN);
+			cnt++;
+		}
+		bw.write(Integer.toString(cnt));
+		bw.close();
+		}
 	
-	Scanner sc = new Scanner(System.in);
-	int N = sc.nextInt();
-	int NewN =  (N % 10)*10 + ((N / 10) + (N % 10)) % 10 ;
-	int tens;
-	int units;
-	int cnt = 1;
-	while (N != NewN)
-	{
-		tens = NewN / 10;
-		units = NewN % 10;
-		NewN = units * 10 + (tens + units) % 10 ;
-		cnt++;
-	}
-	System.out.println(cnt);
-	}
 }
