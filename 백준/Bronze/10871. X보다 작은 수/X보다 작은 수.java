eda20d84899a9e478c5a7e18b9d641d1e714c021
chech2 <90683516+chech2@users.noreply.github.com>
2023-01-27 21:38:07 +0900

    [Bronze V] Title: X보다 작은 수, Time: 212 ms, Memory: 16768 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -1,12 +1,15 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int N = Integer.parseInt(st.nextToken());
@@ -16,10 +19,10 @@ public class Main {
 		for (int i = 0; i < N; i++) {
 			int now = Integer.parseInt(st.nextToken());
 			if(now < X)
-				System.out.print(now + " ");
+				bw.write(now + " "); //.write(int) -> int는 char타입 숫자코드 의미, string로 바꿔주기
+
 		}
-		System.out.println();
-		
+		bw.close();		
 		}
 		
 }
