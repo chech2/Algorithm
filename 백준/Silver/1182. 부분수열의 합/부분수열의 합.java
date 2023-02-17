4231306cf74407a949bb5784e52f86c7b3af099c
chech2 <90683516+chech2@users.noreply.github.com>
2023-02-17 10:37:22 +0900

    [Silver II] Title: 부분수열의 합, Time: 196 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -2,6 +2,11 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/*
+ 4 0
+-7 -3 -2 5 8
+ */
+
 public class Main {
 	static int N;
 	static int S;
@@ -27,11 +32,9 @@ public class Main {
 	
 	private static void subset() {
 		for (int i = 1; i < (1 << N); i++) {
-			result = new int [N];
-			for (int j = 0; j < N; j++) if((i & 1 << j) != 0) result[j] = arr[j];
 			int now = 0;
-			for (int k = 0; k < N; k++) now += result[k];
-				if (S == now) totalCnt++;
+			for (int j = 0; j < N; j++) if((i & 1 << j) != 0) now += arr[j];
+			if (S == now) totalCnt++;
 		}
 	}
 }
