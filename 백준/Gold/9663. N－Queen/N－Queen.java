029b45dc68ee8f38d2d314d512c5940c3e26a486
chech2 <90683516+chech2@users.noreply.github.com>
2023-04-17 22:55:58 +0900

    [Gold IV] Title: N-Queen, Time: 5208 ms, Memory: 18656 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,17 +1,20 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 //퀸의 이동 가능 방향, 4방, 대각선
 // 이동이 가능한지 체크 (가로, 세로, 대각선 X) => 가능하면 놓고 불가능하면 백트레킹
 
 public class Main {
-    static ArrayList<Integer> chessX = new ArrayList<>();
-    static ArrayList<Integer> chessY = new ArrayList<>();
+    static int[] chess;
+//    static ArrayList<Integer> chessY = new ArrayList<>();
 
     static int N, result;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
+        chess = new int[N];
+        Arrays.fill(chess, -1);
         dfs(0);
         System.out.println(result);
     }
@@ -24,23 +27,24 @@ public class Main {
 
         //depth 행에서 1개 선택하기
         for (int j = 0; j < N; j++) {
-            if (!chessY.contains(j) && dia(depth, j)) {
-                chessX.add(depth);
-                chessY.add(j);
+            if (dia(depth, j)) {
+                chess[depth] = j;
                 dfs(depth + 1);
-                int x = chessX.indexOf(depth);
-                int y = chessY.indexOf(j);
-                chessX.remove(x);
-                chessY.remove(y);
+                chess[depth] = -1;
             }
         }
     }
-    
+
     static boolean dia(int r, int c){
+        //열 체크
+        for (int i = 0; i < N; i++) {
+            if(chess[i] == c) return false;
+        }
+
         //대각선 체크
-        for (int i = 0; i < chessX.size(); i++) {
-            if(Math.abs(r - chessX.get(i)) == Math.abs(c - chessY.get(i)))
-                return false;
+        for (int i = 0; i < N; i++) {
+            if(chess[i] == -1)continue;
+            if(Math.abs(r - i) == Math.abs(c - chess[i])) return false;
         }
         return true;
     }
