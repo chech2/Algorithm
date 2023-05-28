3c568ba5b9a7ba3b220a5b3dbe26f8b6634321de
chech2 <90683516+chech2@users.noreply.github.com>
2023-05-28 11:57:17 +0900

    [Silver V] Title: 돌 게임, Time: 220 ms, Memory: 17800 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    static int N;
+    static int memo[];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        memo = new int[N + 1];
+        Arrays.fill(memo, -1);
+        memo[0] = 0;
+        memo[1] = 1;
+
+        memo[N] = dp(N);
+        if(memo[N] % 2 == 0) System.out.println("CY");
+        else System.out.println("SK");
+    }
+
+    static int dp(int n){
+        if(n < 0) return Integer.MAX_VALUE;
+        if(memo[n] == -1){
+            memo[n] = Math.min(dp(n - 3), dp(n - 1)) + 1;
+        }
+        return memo[n];
+    }
+}
