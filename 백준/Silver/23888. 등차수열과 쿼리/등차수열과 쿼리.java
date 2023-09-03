36a01b6cc9ac2181474b7495fdfb23a9a95ab6d8
chech2 <90683516+chech2@users.noreply.github.com>
2023-09-03 22:07:12 +0900

    [Silver I] Title: 등차수열과 쿼리, Time: 304 ms, Memory: 17984 KB -BaekjoonHub

--- 백준/Silver/23888. 등차수열과 쿼리/등차수열과 쿼리.java
+++ 백준/Silver/23888. 등차수열과 쿼리/등차수열과 쿼리.java
@@ -5,7 +5,8 @@ import java.util.StringTokenizer;
 public class Main {
 
     //순서대로: 초항, 공차, 쿼리 개수, 시작 항, 끝 항, 명령어, 결과
-    static long a, d, q, start, end, command, result;
+    static long a, d, q, start, end, command;
+    static long[] result = new long[2];
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -13,27 +14,29 @@ public class Main {
         a = Long.parseLong(st.nextToken());
         d = Long.parseLong(st.nextToken());
         q = Long.parseLong(br.readLine());
+
         for (int i = 0; i < q; i++) {
             st = new StringTokenizer(br.readLine());
             command = Long.parseLong(st.nextToken());
             start = Long.parseLong(st.nextToken());
             end = Long.parseLong(st.nextToken());
-            result = 0;
 
             //end - start까지의 합 구하기
             if(command == 1){
+                result[0] = 0;
                 long n = end - start + 1;
-                result = (n * (cal(start) + cal(end))) / 2;
+                result[0] = (n * (cal(start) + cal(end))) / 2;
+                System.out.println(result[0]);
             }
             else{
-                if(start == end) result = cal(start);
-                else result = gcd(a, d);
+                if(start == end)
+                    System.out.println(cal(start));
+                else{
+                    if(result[1] == 0) result[1] = gcd(a, d);
+                    System.out.println(result[1]);
+                }
             }
-            System.out.println(result);
         }
-
-
-
     }
 
     static long cal(long num){
