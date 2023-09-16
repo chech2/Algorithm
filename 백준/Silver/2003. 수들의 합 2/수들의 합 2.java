e596d8a894efb38af27c6dd62377d2c8d46c6c10
chech2 <90683516+chech2@users.noreply.github.com>
2023-09-16 12:10:40 +0900

    [Silver IV] Title: 수들의 합 2, Time: 168 ms, Memory: 15416 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -1,46 +1,44 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M, ans;
-    static int [] numbers;
-    static boolean visited[];
-    public static void main(String[] args) throws Exception {
+    static int N, NUM, cnt, start, end, check;
+    static int[] arr;
+
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        numbers = new int[N];
-        visited = new boolean[N];
+        NUM = Integer.parseInt(st.nextToken());
+        end = 0;
+        arr = new int[N];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            numbers[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        for (int i = 1; i <= N ; i++) {
-            subset(i);
-        }
-        System.out.println(ans);
-    }
-
-    static void subset(int size){
-        int result = 0, start = 0, cnt = 0;
-        for (int i = 0; i < N; i++) {
-            cnt++;
-            if(cnt < size)
-                result += numbers[i];
-            else{
-                result += numbers[i];
-                if(result == M) ans++;
-                result -= numbers[start];
-
+        check = arr[start];
+        while (true){
+            if(check < NUM) {
+                end++;
+                if(N <= end) break;
+                check += arr[end];
+            }else if(NUM < check){
+                check -= arr[start];
                 start++;
+            } else if (NUM == check) {
+                cnt++;
+                start++;
+                end = start;
+                if(N <= start || N <= end) break;
+                check = arr[start];
+
             }
         }
+        System.out.println(cnt);
 
     }
+
+
 }
