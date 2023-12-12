893737cf0523c045497776d3587c749e6b9c7e37
Leechaelim <chaelim3396@gmail.com>
2023-12-12 15:00:54 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 1456 ms, Memory: 41272 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -1,41 +1,39 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
+import java.util.stream.Stream;
 
 public class Main {
 
-    static int N, M, K, ans, sX, sY, eX, ey;
-    static int[][] arr;
-    public static void main(String[] args) throws Exception {
+    static int N, M, startX, startY, endX, endY, K;
+    static long[][] arr;
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
-        arr = new int[N + 1][M + 1];
-        for (int i = 1; i < N + 1; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 1; j < M + 1; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
+        arr = new long[N][M];
+        
+        for (int i = 0; i < N; i++) {
+            arr[i] = Stream.of(br.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
         }
+
         K = Integer.parseInt(br.readLine());
-        for (int k = 0; k < K; k++) {
-            ans = 0;
+
+        for (int i = 0; i < K; i++) {
             st = new StringTokenizer(br.readLine());
-            sX = Integer.parseInt(st.nextToken());
-            sY = Integer.parseInt(st.nextToken());
-            eX = Integer.parseInt(st.nextToken());
-            ey = Integer.parseInt(st.nextToken());
-            for (int i = sX; i <= eX; i++) {
-                for (int j = sY; j <= ey; j++) {
-                    ans += arr[i][j];
+            startX = Integer.parseInt(st.nextToken()) - 1;
+            startY = Integer.parseInt(st.nextToken()) - 1;
+            endX = Integer.parseInt(st.nextToken()) - 1;
+            endY = Integer.parseInt(st.nextToken()) - 1;
+            long sum = 0;
+            for (int j = startX; j <= endX; j++) {
+                for (int k = startY; k <= endY; k++) {
+                    sum += arr[j][k];
                 }
             }
-            System.out.println(ans);
+            System.out.println(sum);
         }
-
     }
 }
