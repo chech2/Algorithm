e60bb755fb3bea6b9857b1dc27c36ad5092e0223
Leechaelim <chaelim3396@gmail.com>
2023-12-17 02:43:47 +0900

    [Gold III] Title: 나머지 합, Time: 508 ms, Memory: 120480 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -1,12 +1,13 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+
     static int N, M;
     static long ans;
-    static long[] modArr, aSum, modCnt;
-
+    static long[] mod, modCnt;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -14,22 +15,19 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
-
+        mod = new long[N + 1];
         modCnt = new long[M];
-        aSum = new long[N + 1];
-        modArr = new long[N + 1];
-        long tmp;
+
+        st = new StringTokenizer(br.readLine());
+        long input = 0;
         for (int i = 1; i < N + 1; i++) {
-            tmp = Long.parseLong(st.nextToken());
-            modArr[i] = (modArr[i - 1] + tmp) % M;
-            if(modArr[i] == 0) ans++;
-            modCnt[(int)modArr[i]]++;
+            input += Long.parseLong(st.nextToken());
+            mod[i] = input % M;
+            if(mod[i] == 0) ans++;
+            modCnt[(int)mod[i]]++;
         }
         for (int i = 0; i < M; i++) {
-            tmp = modCnt[i];
-            if(1 < tmp)
-            ans += (modCnt[i] * (modCnt[i] - 1))/ 2;
+            ans += (modCnt[i] * (modCnt[i] - 1)) / 2;
         }
         System.out.println(ans);
     }
