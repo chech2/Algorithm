9ae1c1b17240e083ff1f24962d42d5548fb4784d
Leechaelim <chaelim3396@gmail.com>
2023-12-23 17:45:53 +0900

    [Silver I] Title: 트리 순회, Time: 120 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,13 +1,11 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
 
-    static ArrayList<node> tree = new ArrayList<>();
     static int N;
+    static StringBuilder[] sb = new StringBuilder[3];
     public static class node{
         char now;
         node left, right;
@@ -45,7 +43,7 @@ public class Main {
         }
         public static void preorder(node node){
             if(node != null) {
-                System.out.print(node.now);
+                sb[0].append(node.now);
                 if (node.left != null) preorder(node.left);
                 if (node.right != null) preorder(node.right);
             }
@@ -53,7 +51,7 @@ public class Main {
         public static void inorder(node node){
             if(node != null) {
                 if (node.left != null) inorder(node.left);
-                System.out.print(node.now);
+                sb[1].append(node.now);
                 if (node.right != null) inorder(node.right);
             }
         }
@@ -61,7 +59,8 @@ public class Main {
             if(node != null) {
                 if (node.left != null) postorder(node.left);
                 if (node.right != null) postorder(node.right);
-                System.out.print(node.now);
+                sb[2].append(node.now);
+
             }
         }
     }
@@ -69,6 +68,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         TREE tree = new TREE();
+        for (int i = 0; i < 3; i++) sb[i] = new StringBuilder();
 
         // 순서대로 각 인덱스가 A ~ N개의 노드를 의미함 (A == 1)
         char[] input;
@@ -77,9 +77,11 @@ public class Main {
             tree.createNode(input[0], input[2], input[4]);
         }
         tree.preorder(tree.root);
-        System.out.println();
         tree.inorder(tree.root);
-        System.out.println();
         tree.postorder(tree.root);
+
+        System.out.println(sb[0]);
+        System.out.println(sb[1]);
+        System.out.println(sb[2]);
     }
 }
