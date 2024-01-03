d1a2721eac1f00445e0555e24426f09fbd94284b
Leechaelim <chaelim3396@gmail.com>
2024-01-03 18:57:08 +0900

    [Silver II] Title: 뒤집어진 소수, Time: 1648 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Silver/10859. 뒤집어진 소수/뒤집어진 소수.java
+++ 백준/Silver/10859. 뒤집어진 소수/뒤집어진 소수.java
@@ -1,46 +1,51 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    static long N, reverse;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextLong();
+    static char[] numbers, reverseNumber;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        numbers = br.readLine().toCharArray();
 
-        //입력값 소수판정하기
-        check(N);
-
-        //숫자 뒤집기
-        long tmp;
-        while (N > 0) {
-            tmp = N % 10;
-            if (tmp == 3 || tmp == 4 || tmp == 7) {
+        if(reverse(numbers)){
+            if (isPrime(numbers) && isPrime(reverseNumber)) {
+                System.out.println("yes");
+            } else {
                 System.out.println("no");
-                System.exit(0);
-            }else if(tmp == 6 || tmp == 9){
-                if(tmp == 6) tmp = 9;
-                else tmp = 6;
             }
-            reverse = reverse * 10 + tmp;
-            N /= 10;
-        }
-
-        //소수 판정
-        check(reverse);
-
-        System.out.println("yes");
-    }
-
-    static void check(long num){
-        if(num == 1){
+        }else {
             System.out.println("no");
-            System.exit(0);
         }
-        for (long i = 2; i <= num / i; i++) {
-            if(num % i == 0){
-                System.out.println("no");
-                System.exit(0);
+
+    }
+    static boolean reverse(char[] number){
+        reverseNumber = new char[number.length];
+
+
+        for (int i = numbers.length - 1, j = 0; 0 <= i; i--, j++) {
+            if(numbers[i] == '3' ||numbers[i] == '4' || numbers[i] == '7') {
+                return false;
+            } else if(numbers[i] == '0' || numbers[i] == '1' ||numbers[i] == '2' ||numbers[i] == '5' ||numbers[i] == '8') {
+                reverseNumber[j] = numbers[i];
+            }
+            else {
+                if(numbers[i] == '6') reverseNumber[j] = '9';
+                else reverseNumber[j] = '6';
             }
         }
+
+        return true;
     }
+
+    static boolean isPrime(char[] number){
+        long num = Long.parseLong(String.valueOf(number));
+        if(num < 2) return false;
+
+        for (long i = 2; i * i <= num; i++) {
+            if(num % i == 0) return false;
+        }
+        return true;
+    }
+
 }
