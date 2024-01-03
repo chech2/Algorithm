df98b32e04f5f87fc411d8800ed5effd2d15c0a8
Leechaelim <chaelim3396@gmail.com>
2024-01-03 17:30:12 +0900

    [Silver I] Title: 보석 도둑, Time: 136 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Silver/14232. 보석 도둑/보석 도둑.java
+++ 백준/Silver/14232. 보석 도둑/보석 도둑.java
@@ -1,27 +1,32 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 
 public class Main {
-    static long K, result;
-    static ArrayList<Integer> list = new ArrayList<>();
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        K = sc.nextLong();
+    static long k, cnt;
+    static ArrayDeque<Long> list = new ArrayDeque<>();
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        k = Long.parseLong(br.readLine());
 
-        for (int i = 2; i < K / i; i++) {
-            while(K % i == 0){
-                K /= i;
+        for(long i = 2; i * i <= k; i++){
+            while (k % i == 0) {
+                k /= i;
+                cnt++;
                 list.add(i);
-                result++;
             }
         }
-        if(K != 1) result++;
-        System.out.println(result);
-        for (int i = 0; i < list.size(); i++) {
-            System.out.print(list.get(i) + " ");
+
+        if (k != 1) {
+            cnt++;
+            list.add(k);
         }
-        if(K != 1) System.out.println(K);
+
+        sb.append(cnt).append("\n");
+        while (!list.isEmpty())sb.append(list.poll()).append(" ");
+        System.out.println(sb);
     }
 
 }
