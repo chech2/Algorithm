9bd6d9077645940770365b9b680ee00ce02c6680
Leechaelim <chaelim3396@gmail.com>
2024-01-21 15:54:51 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 144 ms, Memory: 15920 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,39 +1,43 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class Main {
-    static boolean[] visited = new boolean[9];
-    static int[] people = new int[9];
+
+    static int[] arr;
+    static int sum;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        arr = new int[9];
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+        Arrays.sort(arr);
+
+        int start = 0;
+        int end = 8;
+        int now = 0;
+
+        while(sum - now != 100){
+            now = arr[start] + arr[end];
+            if(100 < sum - now){
+                start++;
+            }else if(sum - now < 100){
+                end--;
+            }
+        }
 
         for (int i = 0; i < 9; i++) {
-            people[i] = Integer.parseInt(br.readLine());
+            if(start == i || end == i) continue;
+            bw.append(arr[i] + "\n");
         }
-        Arrays.sort(people);
-        combi(0, 0);
-
-    }
-
-    public static void combi(int start, int cnt){
-        if(cnt == 7){
-            int sum = 0;
-            for (int i = 0; i < 9; i++) {
-                if(visited[i]) sum += people[i];
-            }
-            if(sum == 100){
-                for (int i = 0; i < 9; i++) {
-                    if(visited[i]) System.out.println(people[i]);
-                }
-                System.exit(0);
-            }
-            return;
-        }
-        for (int i = start; i < 9; i++) {
-            visited[i] = true;
-            combi(i + 1, cnt + 1);
-            visited[i] = false;
-        }
+        bw.flush();
+        bw.close();
     }
 }
