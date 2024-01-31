e62b0f91de47f3e0f28f79ac0ee47d4ebba67169
Leechaelim <chaelim3396@gmail.com>
2024-01-31 14:02:39 +0900

    [Silver III] Title: N과 M (1), Time: 2256 ms, Memory: 62076 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,46 +1,43 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int N;
-	static int M;
-	static int[] numbers;
-	static int[] result;
-	static boolean[] isSelected;
-	
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		N = sc.nextInt();
-		M = sc.nextInt();
-		
-		numbers = new int[N];
-		result = new int[M + 1];
-		isSelected = new boolean[N];
-		
-		for (int i = 0; i < N; i++) {
-			numbers[i] = i + 1;
-		}
-		perm(0);
-		
-	}
 
-	private static void perm(int cnt) {
-		if(cnt == M) {
-			for (int i = 0; i < M; i++) {
-				System.out.print(result[i] + " ");
-			}
-			System.out.println();
-			return;
-		}
-		for (int i = 0; i < N; i++) {
-			if (isSelected[i]) continue;
-			isSelected[i] = true;
-			result[cnt] = numbers[i];
-			perm(cnt + 1);
-			isSelected[i] = false;
-		}
-		
-	}
+    static int n, m;
+    static int[] seleted;
+    static boolean[] visited;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        seleted = new int[m];
+        visited = new boolean[n + 1];
+
+        perm(0);
+    }
+
+    public static void perm(int cnt){
+        if(cnt == m){
+            for (int i = 0; i < m; i++) {
+                System.out.print(seleted[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if(visited[i]) continue;
+            visited[i] = true;
+            seleted[cnt] = i;
+            perm(cnt + 1);
+            visited[i] = false;
+        }
+    }
 }
