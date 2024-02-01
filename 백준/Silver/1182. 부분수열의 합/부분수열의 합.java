6c71fc4285e0d86555f5ce75ccd3e07e98c83773
Leechaelim <chaelim3396@gmail.com>
2024-02-01 20:17:08 +0900

    [Silver II] Title: 부분수열의 합, Time: 132 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -2,39 +2,37 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/*
- 4 0
--7 -3 -2 5 8
- */
-
 public class Main {
-	static int N;
-	static int S;
-	static int totalCnt;
-	static int arr[];
-	static int result[];
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		S = Integer.parseInt(st.nextToken());
-		totalCnt = 0;
-		arr = new int [N];
-		
-		st = new StringTokenizer(br.readLine());
-		
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		subset();
-		System.out.println(totalCnt);
-	}
-	
-	private static void subset() {
-		for (int i = 1; i < (1 << N); i++) {
-			int now = 0;
-			for (int j = 0; j < N; j++) if((i & 1 << j) != 0) now += arr[j];
-			if (S == now) totalCnt++;
-		}
-	}
+
+    static int n, s, ans;
+    static int[] number;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        s = Integer.parseInt(st.nextToken());
+
+        number = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            number[i] = Integer.parseInt(st.nextToken());
+        }
+
+        subset(1, number[0]);
+        subset(1, 0);
+
+        if(s == 0) ans--;
+        System.out.println(ans);
+    }
+    public static void subset(int cnt, int num){
+        if(cnt == n){
+            if(s == num) ans++;
+            return;
+        }
+
+        subset(cnt + 1, num + number[cnt]);
+        subset(cnt + 1, num);
+    }
 }
