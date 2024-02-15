853371119bb61d5f4f4e09bd3424b5675ab19f3e
Leechaelim <chaelim3396@gmail.com>
2024-02-15 22:14:24 +0900

    [Silver II] Title: 제곱수의 합, Time: 3100 ms, Memory: 28428 KB -BaekjoonHub

--- 백준/Silver II/1699. 제곱수의 합/제곱수의 합.java
+++ 백준/Silver II/1699. 제곱수의 합/제곱수의 합.java
@@ -1,33 +1,33 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
-    static int n, ans;
+    static int n, size;
     static ArrayList<Integer> list = new ArrayList<>();
     static int[] dp;
 
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        ans = Integer.MAX_VALUE;
         dp = new int[n];
+        Arrays.fill(dp, -1);
         setting(n);
 
-        Arrays.fill(dp, -1);
-
-        System.out.println(recur(0));
+        size = list.size();
+        System.out.println(recur(0, 0));
     }
 
-    public static int recur(int num){
-        int result = 200000;
+    public static int recur(int num, int start){
         if(n < num) return 200000;
         if(num == n) return 0;
         if(dp[num] != -1) return dp[num];
 
-        for (int i = 0; i < list.size(); i++) {
-            result = Math.min(recur(num + list.get(i)) + 1, result);
+        int result = 200000;
+        for (int i = start; i < size; i++) {
+            result = Math.min(recur(num + list.get(i), i) + 1, result);
         }
         return dp[num] = result;
     }
@@ -39,5 +39,4 @@ public class Main {
             list.add(i * i);
         }
     }
-
 }
