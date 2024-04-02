028d80ba8f8e5c084e07be4dc166f80e39065382
Leechaelim <chaelim3396@gmail.com>
2024-04-02 19:13:58 +0900

    [Silver I] Title: RGB거리, Time: 140 ms, Memory: 14756 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,61 +1,43 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.nio.Buffer;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int n;
-    static int[][] house, dp;
+    static int n, ans;
+    static int[][] map, dp;
+    static StringTokenizer st;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        dp = new int[n][3];
-        house = new int[n][3];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-
-        StringTokenizer st;
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 3; j++) {
-                house[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        recur(0,  -1);
-        System.out.println(Math.min(dp[0][0], Math.min(dp[0][1], dp[0][2])));
+        input();
+        System.out.println(recur(0, -1));
     }
 
     public static int recur(int cnt, int pre){
         if(cnt == n) return 0;
-        int ret = isSave(cnt, pre);
-        if(ret != -1) return ret;
-
-        ret = 100000000;
+        int ret = 1000000;
         for (int i = 0; i < 3; i++) {
             if(i == pre) continue;
-            dp[cnt][i] = recur(cnt + 1, i) + house[cnt][i];
+            if(dp[cnt][i] == 0) dp[cnt][i] = recur(cnt + 1, i) + map[cnt][i];
             ret = Math.min(ret, dp[cnt][i]);
         }
         return ret;
     }
-
-    public static int isSave(int cnt, int pre){
-        if(pre == 0){
-            if(dp[cnt][1] != -1 && dp[cnt][2] != -1) {
-                return Math.min(dp[cnt][1], dp[cnt][2]);
-            }
-        }else if(pre == 1){
-            if(dp[cnt][0] != -1 && dp[cnt][2] != -1) {
-                return Math.min(dp[cnt][0], dp[cnt][2]);
-            }
-
-        }else if(pre == 2){
-            if(dp[cnt][0] != -1 && dp[cnt][1] != -1) {
-                return Math.min(dp[cnt][0], dp[cnt][1]);
+    public static void input() throws Exception{
+        strToken();
+        n = Integer.parseInt(st.nextToken());
+        map = new int[n][3];
+        dp = new int[n][3];
+        ans = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            strToken();
+            for (int j = 0; j < 3; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        return -1;
+    }
+    public static void strToken() throws Exception{
+        st = new StringTokenizer(br.readLine());
     }
 }
