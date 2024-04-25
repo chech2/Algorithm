d094c5af915c3e6b062d59136f9905747b4fc630
Leechaelim <chaelim3396@gmail.com>
2024-04-25 16:52:24 +0900

    [Bronze II] Title: Farm, Time: 124 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Bronze/16283. Farm/Farm.java
+++ 백준/Bronze/16283. Farm/Farm.java
@@ -1,22 +1,40 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
+import java.util.*;
+import java.io.*;
 public class Main {
-    static int a, b, n, w;
-    static ArrayList<String> list = new ArrayList<>();
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        a = sc.nextInt();   // 양 한마리가 먹는 사료
-        b = sc.nextInt();   // 염소 한마리가 먹는 사료
-        n = sc.nextInt();   // 총 마리수
-        w = sc.nextInt();   // 소비된 총 사료
+    static int a, b, n, w, sheep, goat, cnt;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    public static void main(String[] args) throws Exception{
+        input();
+    }
 
-        // i = 양의 수, j = 염소의 수
-        for (int i = 1, j = n - 1; i < n; i++, j--) {
-            if (i * a + j * b == w) list.add(i + " " + j);
+    public static void strToken() throws Exception{
+        st = new StringTokenizer(br.readLine());
+    }
+
+    public static void input() throws Exception{
+        strToken();
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        w = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i < n; i++) {
+            if(a * i + b * (n - i) == w) {
+                if(sheep != 0){
+                    sheep = 0;
+                    break;
+                }else{
+                    sheep = i;
+                    goat = n - i;
+                }
+            }
         }
+        if(sheep == 0) bw.append("-1");
+        else bw.append(String.valueOf(sheep)).append(" ").append(String.valueOf(goat));
+        bw.flush();
+        bw.close();
 
-        if(list.size() == 1) System.out.println(list.get(0));
-        else System.out.println(-1);
     }
 }
