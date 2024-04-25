33e8162b63a68ae26044916e225b5add924ee79d
Leechaelim <chaelim3396@gmail.com>
2024-04-25 17:41:21 +0900

    [Bronze III] Title: 2017 연세대학교 프로그래밍 경시대회, Time: 132 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.java
+++ 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.java
@@ -1,35 +1,26 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.nio.Buffer;
 
 public class Main {
-    static int N, totalcnt;
-    static int[] number, result;
+    static int a, b, c, candy, cnt;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-         N = Integer.parseInt(br.readLine());
-         number = new int[N];
-         result = new int[3];
+        candy = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < N; i++) {
-            number[i] = i + 1;
-        }
-
-        permutation(0, 0);
-        System.out.println(totalcnt);
-    }
-
-    public static void permutation(int cnt, int sum){
-        if(cnt == 3){
-            if(sum == N){
-                if(result[0] % 2 == 0 && result[1] + 2 <= result[2]){ // 택희의 사탕 수는 짝수
-                        totalcnt++;
+        // i: 남규, j: 영훈, k: 택희
+        // 택희는 짝수, 남규는 영훈보다 2개 많기
+        for (int i = 1; i < candy - 2; i++) {
+            for (int j = 1; j < candy - 2; j++) {
+                for (int k = 2; k < candy - 2; k += 2) {
+                    if(k % 2 == 1) continue;
+                    if(i < j + 2) continue;
+                    if(k + i + j != candy) continue;
+                    cnt++;
                 }
             }
-            return;
         }
-        for (int i = 0; i < N; i++) {
-            result[cnt] = number[i];
-            permutation(cnt + 1, sum + number[i]);
-        }
+        System.out.println(cnt);
     }
 }
