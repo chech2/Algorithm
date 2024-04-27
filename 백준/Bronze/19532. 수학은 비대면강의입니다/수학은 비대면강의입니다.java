0522d5170ea73fc81e9a7d97dddf324d2bbdbb35
Leechaelim <chaelim3396@gmail.com>
2024-04-27 21:47:22 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 164 ms, Memory: 16056 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -1,20 +1,28 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-
+import java.util.*;
+import java.io.*;
 public class Main {
-    static int[] input;
+
+    static int a, b, c, d, e, f;
+    static StringTokenizer st;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer :: parseInt).toArray();
+        st = new StringTokenizer(br.readLine());
+
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        d = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+        f = Integer.parseInt(st.nextToken());
 
         for (int i = -999; i < 1000; i++) {
             for (int j = -999; j < 1000; j++) {
-                if(input[0] * i + input[1] * j == input[2] && input[3] * i + input[4] * j == input[5]){
+                if(a * i + b * j == c && d * i + e * j == f) {
                     System.out.println(i + " " + j);
-                    break;
+                    return;
                 }
             }
         }
     }
+
 }
