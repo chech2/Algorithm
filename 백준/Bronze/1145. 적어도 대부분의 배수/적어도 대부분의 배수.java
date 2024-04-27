c63265ad9ae27b77acac8a3f8b6ad9effb471ec8
Leechaelim <chaelim3396@gmail.com>
2024-04-27 21:19:15 +0900

    [Bronze I] Title: 적어도 대부분의 배수, Time: 152 ms, Memory: 14472 KB -BaekjoonHub

--- 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.java
+++ 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.java
@@ -1,33 +1,39 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.stream.Stream;
-
+import java.util.*;
+import java.io.*;
 public class Main {
-
-    static int[] input = new int[5];
-    static int maxNum, minNum, cnt;
+    static int ans, maxv, minv;
+    static int[] arr;
+    static StringTokenizer st;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer :: parseInt).toArray();
+        input();
+        tmp();
+        System.out.println(ans);
+    }
 
+    public static void input() throws Exception{
+        st = new StringTokenizer(br.readLine());
+        arr = new int[5];
 
-        Arrays.sort(input);
-        maxNum = input[4] * input[3] * input[2];
-        minNum = input[0];
+        for (int i = 0; i < 5; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        maxv = arr[2] * arr[3] * arr[4];
+        minv = arr[0];
+    }
 
-        for (int i = minNum; i <= maxNum; i++) {
-
+    public static void tmp(){
+        int cnt;
+        for (int i = minv; i <= maxv; i++) {
             cnt = 0;
             for (int j = 0; j < 5; j++) {
-                if(i % input[j] == 0) cnt++;
+                if(i % arr[j] == 0) cnt++;
             }
-            if(3 <= cnt) {
-                System.out.println(i);
+            if(2 < cnt) {
+                ans = i;
                 break;
             }
         }
-
     }
 }
