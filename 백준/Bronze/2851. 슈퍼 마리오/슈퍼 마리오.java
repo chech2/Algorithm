f2105be7cd8372747a0e312d311d394816bcb608
Leechaelim <chaelim3396@gmail.com>
2024-04-27 20:32:43 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 120 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
@@ -1,30 +1,28 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-
+import java.util.*;
+import java.io.*;
 public class Main {
-    static int[] input = new int[11];
+
+    static int result;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(input());
+    }
 
-        for (int i = 1; i < 11; i++) {
-            input[i] = input[i - 1] + Integer.parseInt(br.readLine());
-        }
+    public static int input() throws Exception{
+        int now, next;
 
-        for (int i = 1; i < 11; i++) {
-            if(input[i] == 100){
-                System.out.println(input[i]);
-                break;
-            }else if(100 < input[i]){
-                if(100 - input[i - 1] < input[i] - 100){
-                    System.out.println(input[i - 1]);
-                }else{
-                    System.out.println(input[i]);
-                }
-                break;
-            }else if(i == 10){
-                System.out.println(input[i]);
-            }
+        for (int i = 0; i < 10; i++) {
+            now = Integer.parseInt(br.readLine());
+            next = result + now;
+
+            if(100 < next)
+                if(Math.abs(100 - result) < Math.abs(100 - next)) return result;
+                else return next;
+
+            else if(next == 100) return next;
+            result = next;
+
         }
+        return result;
     }
 }
