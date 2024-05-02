5236ce995201567695a6e0f06539771731f8ab8f
Leechaelim <chaelim3396@gmail.com>
2024-05-02 15:21:18 +0900

    [Silver III] Title: N과 M (4), Time: 140 ms, Memory: 15512 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,51 +1,40 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int result[], number[];
-    static int N, M;
+    static int n, m;
+    static char[] selected;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        number = new int[N];
-        result = new int[M];
-
-        for (int i = 0; i < N; i++) {
-            number[i] = i + 1;
-        }
-
-        perm(0);
+    static StringTokenizer st;
+    public static void main(String[] args) throws Exception{
+        input();
+        combi(1, 0);
         System.out.println(sb);
     }
 
-    static void perm(int cnt){
-        if(cnt == M){
-            for (int i = 0; i < M; i++) {
-                sb.append(result[i]).append(" ");
-            }
-            sb.append("\n");
-            return;
-        }
-        for (int i = 0; i < N; i++) {
-            if(cnt == 0) {
-                result[cnt] = number[i];
-            }else{
-                for (int j = i; j < N; j++) {
-                    if(result[cnt - 1] <= number[j]) {
-                        result[cnt] = number[j];
-                        i = j;
-                        break;
-                    }
-                }
-            }
-            perm(cnt + 1);
+    public static void input() throws Exception{
+        st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        selected = new char[2 * m];
+        selected[2 * m - 1] = '\n';
+        for (int i = 1; i < 2 * m - 1; i += 2) {
+            selected[i] = ' ';
         }
     }
+
+    public static void combi(int start, int cnt){
+        if(cnt == m){
+            sb.append(selected);
+            return;
+        }
+        for (int i = start; i < n + 1; i++) {
+            selected[2 * cnt] = (char)(i + '0');
+            combi(i, cnt + 1);
+        }
+    }
+
 }
