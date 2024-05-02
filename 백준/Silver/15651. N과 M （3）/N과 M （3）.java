b84250dc715d57488d3ee5022ee6923ce4837e3d
Leechaelim <chaelim3396@gmail.com>
2024-05-02 15:04:48 +0900

    [Silver III] Title: N과 M (3), Time: 404 ms, Memory: 68272 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,36 +1,37 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.util.*;
+import java.io.*;
 public class Main {
 
     static int n, m;
-    static int[] selected;
+    static char[] selected;
+    static char[] number = {'1', '2', '3', '4', '5', '6', '7'};
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
     public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        selected = new int[m];
-        dup_per(0);
-        System.out.println(sb);
+        selected  = new char[m];
 
+        perm(0);
+        System.out.println(sb);
     }
 
-    public static void dup_per(int cnt){
+    public static void perm(int cnt) throws Exception{
         if(cnt == m){
             for (int i = 0; i < m; i++) {
-                sb.append(selected[i]).append(" ");
+                sb.append(selected[i]).append(' ');
             }
-            sb.append("\n");
+            sb.append('\n');
             return;
         }
-        for (int i = 1; i <= n; i++) {
-            selected[cnt] = i;
-            dup_per(cnt + 1);
+
+        for (int i = 1; i < n + 1; i++) {
+            selected[cnt] = number[i - 1];
+            perm(cnt + 1);
         }
     }
-
 }
