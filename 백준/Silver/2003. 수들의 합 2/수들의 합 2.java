3e780c07564bcb52d3cf9792b4d5bf326811cc50
Leechaelim <chaelim3396@gmail.com>
2024-06-05 18:09:39 +0900

    [Silver IV] Title: 수들의 합 2, Time: 160 ms, Memory: 15304 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -1,44 +1,51 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 public class Main {
-    static int N, NUM, cnt, start, end, check;
-    static int[] arr;
-
+    static int n, m;
+    static int[] arr, s;
+    static StringTokenizer st;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        NUM = Integer.parseInt(st.nextToken());
-        end = 0;
-        arr = new int[N];
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        check = arr[start];
-        while (true){
-            if(check < NUM) {
-                end++;
-                if(N <= end) break;
-                check += arr[end];
-            }else if(NUM < check){
-                check -= arr[start];
-                start++;
-            } else if (NUM == check) {
-                cnt++;
-                start++;
-                end = start;
-                if(N <= start || N <= end) break;
-                check = arr[start];
-
-            }
-        }
-        System.out.println(cnt);
-
+        input();
+        System.out.println(towPoint());
     }
 
+    public static int towPoint() {
+        int start, end, cnt, sum;
+        start = 0;
+        end = 1;
+        cnt = 0;
+//        System.out.println(Arrays.toString(s));
+        while(start < n){
+            sum = s[end] -  s[start];
+//            System.out.println(start + " " + end + "  >>  " + sum);
+
+            if(sum < m){
+                if(end < n) end++;
+                else start++;
+            }else if(m < sum){
+                start++;
+            }else{
+                cnt++;
+                if(end < n) end++;
+                else start++;
+            }
+        }
+        return cnt;
+    }
+
+    public static void input() throws Exception{
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        s = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            s[i + 1] = arr[i] + s[i];
+        }
+    }
 
 }
