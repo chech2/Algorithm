d7fa7f743397ac08bd6a743127f1c3b825cbb0ca
Leechaelim <chaelim3396@gmail.com>
2024-07-02 03:21:44 +0900

    [Silver III] Title: 바이러스, Time: 96 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,58 +1,53 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	private static void dfs(ArrayList<Integer> grap, int idx) {
-		visidfs[idx] = true;
-		nagali++;
-		//sb.append(idx).append(" ");
-		for (int i = 0 ; i < grap.size(); i++) {
-			if(visidfs[grap.get(i)]) continue;
-			dfs((ArrayList<Integer>) Nodes[grap.get(i)], grap.get(i)); 
-		}   
-	}
-	
-	static void make_grap() throws Exception {
-		int from, to;
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			from = Integer.parseInt(st.nextToken());
-			to = Integer.parseInt(st.nextToken());
-			Nodes[from].add(to);
-			Nodes[to].add(from);
-		} 
-	}
-	
-	static boolean visidfs[];
-	static ArrayList<Integer>[] Nodes;
-	static StringTokenizer st;
-	static int N; //정점의 개수
-	static int M;
-	static int nagali; //바이러스 걸린 컴퓨터 개수
-	static StringBuilder sb = new StringBuilder();
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	public static void main(String[] args) throws Exception {
-		
-		
-		N = Integer.parseInt(br.readLine());
-		M = Integer.parseInt(br.readLine());
+    static int n, m, ans;
+    static boolean[] visited;
+    static ArrayList<Integer>[] graph;
+    static StringTokenizer st;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception {
+        input();
+        dfs(1);
+        System.out.println(ans);
+    }
 
-		Nodes = new ArrayList[N + 1];
-		visidfs = new boolean[N + 2];
-		
-		for (int i = 0; i < N + 1; i++)	Nodes[i] = new ArrayList<>();
-		make_grap(); //그래프 만들기
-		
-		for (int i = 0; i < N + 1; i++) {
-			Collections.sort(Nodes[i]);
-		}
-		
-		dfs((ArrayList<Integer>)Nodes[1], 1);
-		//sb.append("\n");
-		System.out.println(nagali - 1); 	
-	}
+    public static void dfs(int now){
+
+        int next;
+        for (int i = 0; i < graph[now].size(); i++) {
+            next = graph[now].get(i);
+
+            if(visited[next]) continue;
+            ans++;
+            visited[next] = true;
+            dfs(next);
+        }
+
+    }
+
+    public static void input() throws Exception{
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+
+        graph = new ArrayList[n + 1];
+        visited = new boolean[n + 1];
+        visited[1] = true;
+        for (int i = 0; i < n + 1; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        int n1, n2;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            n1 = Integer.parseInt(st.nextToken());
+            n2 = Integer.parseInt(st.nextToken());
+
+            graph[n1].add(n2);
+            graph[n2].add(n1);
+        }
+    }
 }
