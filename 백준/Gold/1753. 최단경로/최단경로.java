85f16e49780fb4a107984c19ffd24fdbdf9c82c1
Leechaelim <chaelim3396@gmail.com>
2024-07-04 18:59:35 +0900

    [Gold IV] Title: 최단경로, Time: 656 ms, Memory: 106760 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,68 +1,78 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-	
-		int V = Integer.parseInt(st.nextToken());
-		int E = Integer.parseInt(st.nextToken());
-		
-		int start = Integer.parseInt(br.readLine()) - 1;
-		ArrayList<ArrayList<int[]>> adjMatrix = new ArrayList<>();
-		
-		for (int i = 0; i < V; i++) {
-			adjMatrix.add(new ArrayList<>());
-		}
-		
-		for (int i = 0; i < E; i++) {
-			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken()) - 1;
-			int to = Integer.parseInt(st.nextToken()) - 1;
-			int W = Integer.parseInt(st.nextToken());
-			
-			adjMatrix.get(from).add(new int[] {to, W});
-		} 
-		
-		int distance[] = new int [V];
-		boolean visited[] = new boolean[V];
+    static int v, e, k;
+    static final int INF = 5000000;
+    static ArrayList<node>[] graph;
+    static int[] d;
+    static PriorityQueue<node> q = new PriorityQueue<>((o1, o2) -> o1.w - o2.w);
+    static StringTokenizer st;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+    static class node{
+        int n, w;
+        node(int n, int w){
+            this.n = n;
+            this.w = w;
+        }
+    }
 
-		Arrays.fill(distance, Integer.MAX_VALUE);
-		distance[start] = 0;
-		for (int i = 0; i < V; i++) {
-			int current = -1;
-			int min = Integer.MAX_VALUE;
-			
-			for (int j = 0; j < V; j++) {
-				if(!visited[j] && min > distance[j]) {
-					min = distance[j];
-					current = j;
-				}
-			}
-			
-			if(current == -1) continue;
-			visited[current] = true;
-						
-			for (int [] now : adjMatrix.get(current)) {
-//				for (int k = 0; k < nowList.size(); k++) {
-					if(!visited[now[0]] && now[1] != 0
-							&& distance[now[0]] > min + now[1]) {
-						distance[now[0]] = min + now[1];
-//					}	
-				}
-								
-			}
-			
-		}
-		for (int i = 0; i < V; i++) {
-			sb.append(distance[i] == Integer.MAX_VALUE ? "INF" : distance[i]).append("\n");
-		}
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws Exception{
+        input();
+        dijkstra();
+
+        for (int i = 1; i < v + 1; i++) {
+            if(d[i] == INF) sb.append("INF");
+            else sb.append(d[i]);
+            sb.append("\n");
+        }
+        bw.append(sb);
+        bw.flush();
+        bw.close();
+    }
+
+    public static void dijkstra(){
+        q.add(new node(k, 0));
+
+        node now, next;
+        while(!q.isEmpty()){
+            now = q.poll();
+
+            for (int i = 0; i < graph[now.n].size(); i++) {
+                next = graph[now.n].get(i);
+
+                if(d[now.n] + next.w < d[next.n]){
+                    d[next.n] = d[now.n] + next.w;
+                    q.add(new node(next.n, d[next.n]));
+                }
+            }
+        }
+    }
+    public static void input() throws Exception{
+        st = new StringTokenizer(br.readLine());
+
+        v = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+        graph = new ArrayList[v + 1];
+        d = new int[v + 1];
+        Arrays.fill(d, INF);
+
+        k = Integer.parseInt(br.readLine());
+        d[k] = 0;
+
+        for (int i = 0; i < v + 1; i++) graph[i] = new ArrayList<>();
+
+        int n1, n2, w;
+        for (int i = 0; i < e; i++) {
+            st = new StringTokenizer(br.readLine());
+            n1 = Integer.parseInt(st.nextToken());
+            n2 = Integer.parseInt(st.nextToken());
+            w = Integer.parseInt(st.nextToken());
+
+            graph[n1].add(new node(n2, w));
+        }
+    }
 }
