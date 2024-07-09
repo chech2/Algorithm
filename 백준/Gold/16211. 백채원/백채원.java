d892c7a92daa73236b5297dee9b82e64a91bc9d5
Leechaelim <chaelim3396@gmail.com>
2024-07-10 04:20:10 +0900

    [Gold II] Title: 백채원, Time: 4476 ms, Memory: 225728 KB, Score: 60 point -BaekjoonHub

--- 백준/Gold/16211. 백채원/백채원.java
+++ 백준/Gold/16211. 백채원/백채원.java
@@ -11,7 +11,8 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws Exception{
         input();
-        for (int i = 0; i < k + 1; i++) dijkstra(people[i], i);
+        dijkstraK();
+        dijkstraWhite();
         print();
     }
 
@@ -19,27 +20,23 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int cnt;
         for (int j = 2; j < n + 1; j++) {
-            cnt = 0;
-            for (int i = 0; i < k; i++) {
-                if(d[k][j] != -1 && d[k][j] < d[i][j]){
-                    cnt++;
-                }
+            if(d[0][j] != -1 && d[0][j] < d[1][j]){
+                sb.append(j).append(" ");
             }
-            if(cnt == k) sb.append(j).append(" ");
         }
+
         if(sb.length() == 0) sb.append(0);
         bw.append(sb);
         bw.flush();
         bw.close();
     }
 
-    public static void dijkstra(int number, int idx){
+    public static void dijkstraWhite(){
         PriorityQueue<node> q = new PriorityQueue<>((o1, o2) -> o1.t - o2.t);
-        q.add(new node(number, 0));
+        q.add(new node(1, 0));
 
-        d[idx][number] = 0;
+        d[0][1] = 0;
         node now, next;
         while(!q.isEmpty()){
             now = q.poll();
@@ -47,8 +44,30 @@ public class Main {
             for (int i = 0; i < graph[now.n].size(); i++) {
                 next = graph[now.n].get(i);
 
-                if(d[idx][now.n] + next.t < d[idx][next.n]){
-                    d[idx][next.n] = d[idx][now.n] + next.t;
+                if(d[0][now.n] + next.t < d[0][next.n]){
+                    d[0][next.n] = d[0][now.n] + next.t;
+                    q.add(next);
+                }
+            }
+        }
+    }
+
+    public static void dijkstraK(){
+        PriorityQueue<node> q = new PriorityQueue<>((o1, o2) -> o1.t - o2.t);
+        for (int i = 0; i < k; i++) {
+            q.add(new node(people[i], 0));
+            d[1][people[i]] = 0;
+        }
+
+        node now, next;
+        while(!q.isEmpty()){
+            now = q.poll();
+
+            for (int i = 0; i < graph[now.n].size(); i++) {
+                next = graph[now.n].get(i);
+
+                if(d[1][now.n] + next.t < d[1][next.n]){
+                    d[1][next.n] = d[1][now.n] + next.t;
                     q.add(next);
                 }
             }
@@ -61,8 +80,8 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
-        people = new int[k + 1];
-        d = new int[k + 1][n + 1]; // 추종자 + 백채원의 최단 경로 저장, d[k]는 백채원
+        people = new int[k];
+        d = new int[2][n + 1]; // 추종자 + 백채원의 최단 경로 저장, d[k]는 백채원
 
         graph = new ArrayList[n + 1];
         for (int i = 0; i < n + 1; i++) graph[i] = new ArrayList<>();
@@ -79,15 +98,13 @@ public class Main {
             graph[n2].add(new node(n1, p));
         }
 
-        Arrays.fill(d[k], 2100000000);
+        Arrays.fill(d[0], 2100000000);
+        Arrays.fill(d[1], 2100000000);
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < k; i++) {
             people[i] = Integer.parseInt(st.nextToken());
-
-            Arrays.fill(d[i], 2100000000);
-            d[k][people[i]] = -1;
+            d[0][people[i]] = -1;
         }
-        people[k] = 1;
     }
 
     // 0: 추종자, 1: 백채원
