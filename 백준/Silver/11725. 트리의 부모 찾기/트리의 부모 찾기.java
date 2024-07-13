ba24a2a024a0c0911a4f924aa073d122d59dff60
Leechaelim <chaelim3396@gmail.com>
2024-07-13 19:15:54 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 492 ms, Memory: 61732 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,59 +1,51 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
-
 public class Main {
-    
-    static int N;
-    static int parent[];
-    static boolean[] visited;
 
-    static ArrayList<Integer>[] list;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-
-        N = Integer.parseInt(br.readLine());
-        list = new ArrayList[N + 1];
-        parent = new int[N + 1];
-        visited = new boolean[N + 1];
-
-        for (int i = 1; i < N + 1; i++){
-            list[i] = new ArrayList<>();
-        }
-
-        for (int i = 0; i < N - 1; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            list[a].add(b);
-            list[b].add(a);
-        }
-
-        Queue<Integer> q = new ArrayDeque<>();
-        visited[1] = true;
-        for (int i = 0; i < list[1].size(); i++) {
-            int now = list[1].get(i);
-            q.add(now);
-            visited[now] = true;
-            parent[now] = 1;
-        }
-        while (!q.isEmpty()){
-            int now = q.poll();
-            int size = list[now].size();
-            for (int i = 0; i < size; i++) {
-                int next = list[now].get(i);
-                if(visited[next]) continue;
-                visited[next] = true;
-                parent[next] = now;
-                q.add(next);
-            }
-        }
-
-        for (int i = 2; i < N + 1; i++) {
-            sb.append(parent[i]).append("\n");
+    static int n;
+    static int[] p;
+    static List<Integer>[] graph;
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception{
+        input();
+        dfs(1, 0);
+        for (int i = 2; i < n + 1; i++) {
+            sb.append(p[i]).append("\n");
         }
         System.out.println(sb);
     }
+
+    public static void dfs(int now, int pre){
+
+        int next;
+        for (int i = 0; i < graph[now].size(); i++) {
+            next = graph[now].get(i);
+            if(next == pre) continue;
+
+            p[next] = now;
+            dfs(next, now);
+        }
+    }
+
+    public static void input () throws Exception{
+        n = Integer.parseInt(br.readLine());
+        p = new int[n + 1];
+        graph = new ArrayList[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        int u, v;
+        for (int i = 0; i < n - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            u = Integer.parseInt(st.nextToken());
+            v = Integer.parseInt(st.nextToken());
+
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+    }
+
 }
