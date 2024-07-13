bde0d4e23c0b429366513bda61aaacdcdef01f33
Leechaelim <chaelim3396@gmail.com>
2024-07-13 19:28:55 +0900

    [Gold V] Title: 트리와 쿼리, Time: 576 ms, Memory: 74184 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
@@ -1,70 +1,63 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
     static int n, r, q;
-    static ArrayList<Integer>[] graph;
-    static int[] sub, ans;
+    static int[] sub, query;
+    static List<Integer>[] graph;
     static StringTokenizer st;
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) throws Exception{
         input();
-        cntsub(r, r);
-
+        dfs(r, 0);
         for (int i = 0; i < q; i++) {
-            sb.append((sub[ans[i]] + 1)).append("\n");
+            sb.append(sub[query[i]]).append("\n");
         }
         System.out.println(sb);
     }
 
-    public static void cntsub(int now, int pre){
+    public static void dfs(int now, int pre){
 
         int next;
         for (int i = 0; i < graph[now].size(); i++) {
             next = graph[now].get(i);
 
             if(next == pre) continue;
-            cntsub(next, now);
-            sub[now] += sub[next] + 1;
+            dfs(next, now);
+            sub[now] += sub[next];
         }
     }
 
-    public static void strToken() throws Exception{
-        st = new StringTokenizer(br.readLine());
-    }
-
     public static void input() throws Exception{
-        strToken();
-
+        st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         r = Integer.parseInt(st.nextToken());
         q = Integer.parseInt(st.nextToken());
 
-        graph = new ArrayList[n + 1];
         sub = new int[n + 1];
-        ans = new int[q];
-        for (int i = 1; i < n + 1; i++) {
+        query = new int[q];
+        Arrays.fill(sub, 1);
+
+        graph = new List[n + 1];
+        for (int i = 0; i < n + 1; i++) {
             graph[i] = new ArrayList<>();
         }
 
         int u, v;
         for (int i = 0; i < n - 1; i++) {
-            strToken();
+            st = new StringTokenizer(br.readLine());
             u = Integer.parseInt(st.nextToken());
             v = Integer.parseInt(st.nextToken());
+
             graph[u].add(v);
             graph[v].add(u);
         }
 
         for (int i = 0; i < q; i++) {
-            strToken();
-            ans[i] = Integer.parseInt(st.nextToken());
+            query[i] = Integer.parseInt(br.readLine());
         }
     }
 
