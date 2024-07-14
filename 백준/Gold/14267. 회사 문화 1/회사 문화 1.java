31abb94a50081603fa0d884b486374dbd3b6621c
Leechaelim <chaelim3396@gmail.com>
2024-07-14 17:26:50 +0900

    [Gold IV] Title: 회사 문화 1, Time: 540 ms, Memory: 68096 KB -BaekjoonHub

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
@@ -1,61 +1,58 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int n, m;
-    static ArrayList<Integer>[] graph;
-    static int[] p, ans;
-    static int[][] info;
+    static int n, m, r;
+    static int[] p, c, ans;
+    static List<Integer>[] graph;
     static StringTokenizer st;
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws Exception{
         input();
-        dfs(1);
+        dfs(r, -1);
+        print();
+    }
+
+    public static void dfs(int now, int pre){
+        int next;
+        for (int i = 0; i < graph[now].size(); i++) {
+            next = graph[now].get(i);
+
+            if(next == pre) continue;
+            ans[next] = ans[now] + c[next];
+            dfs(next, now);
+        }
+    }
+
+    public static void print(){
         for (int i = 1; i < n + 1; i++) {
             sb.append(ans[i]).append(" ");
         }
         System.out.println(sb);
     }
 
-    public static void dfs(int now) {
-        int next;
-        for (int i = 0; i < graph[now].size(); i++) {
-            next = graph[now].get(i);
-            ans[next] += ans[now];
-            dfs(next);
-        }
-    }
-    public static void input() throws Exception{
-        strToken();
+    public static void input () throws Exception{
+        st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
         p = new int[n + 1];
+        c = new int[n + 1];
         ans = new int[n + 1];
+        graph = new List[n + 1];
 
-        info = new int[m][2];
-        graph = new ArrayList[n + 1];
-
-        for (int i = 0; i < n + 1; i++) {
-            graph[i] = new ArrayList<>();
-        }
-
-        strToken();
+        st = new StringTokenizer(br.readLine());
         for (int i = 1; i < n + 1; i++) {
+            graph[i] = new ArrayList<>();
             p[i] = Integer.parseInt(st.nextToken());
 
-            if (p[i] != -1) graph[p[i]].add(i);
+            if(p[i] == -1) r = i;
+            else graph[p[i]].add(i);
         }
 
         for (int i = 0; i < m; i++) {
-            strToken();
-            info[i][0] = Integer.parseInt(st.nextToken());
-            info[i][1] = Integer.parseInt(st.nextToken());
-            ans[info[i][0]] += info[i][1];
+            st = new StringTokenizer(br.readLine());
+            c[Integer.parseInt(st.nextToken())] += Integer.parseInt(st.nextToken());
         }
-    }
-    public static void strToken() throws Exception{
-        st = new StringTokenizer(br.readLine());
     }
 }
