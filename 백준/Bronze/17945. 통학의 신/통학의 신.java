796237e82d4994829c8d790517e5e0d249c0b3ba
Leechaelim <chaelim3396@gmail.com>
2024-08-07 15:08:25 +0900

    [Bronze III] Title: 통학의 신, Time: 108 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/17945. 통학의 신/통학의 신.java
+++ 백준/Bronze/17945. 통학의 신/통학의 신.java
@@ -1,24 +1,22 @@
-import java.util.*;
-import java.io.*;
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
     static int a, b;
     static StringTokenizer st;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
+    
     public static void main(String[] args) throws Exception{
         st = new StringTokenizer(br.readLine());
         a = Integer.parseInt(st.nextToken());
         b = Integer.parseInt(st.nextToken());
 
-        for (int i = -1000; i < 10001; i++) {
-            if(findAns(i)) sb.append(i).append(" ");
+        for (int i = -1000; i < 1001; i++) {
+            if(i*i + 2*i*a + b == 0) sb.append(i).append(" ");
         }
         System.out.println(sb);
     }
 
-    public static boolean findAns(int x){
-        if(x * x + 2 * a * x + b == 0) return true;
-        return false;
-    }
 }
