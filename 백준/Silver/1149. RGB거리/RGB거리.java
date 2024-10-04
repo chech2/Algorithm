e83f71d6f6b00bac9aa2931a8c53a327f2fd69ee
Leechaelim <chaelim3396@gmail.com>
2024-10-05 03:57:16 +0900

    [Silver I] Title: RGB거리, Time: 116 ms, Memory: 14780 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,43 +1,47 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.nio.Buffer;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 public class Main {
 
-    static int n, ans;
-    static int[][] map, dp;
-    static StringTokenizer st;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static int n;
+    static int[][] map, memo;
     public static void main(String[] args) throws Exception{
         input();
-        System.out.println(recur(0, -1));
+        for (int i = 0; i < 3; i++) {
+            recur(0, i);
+//            System.out.println(recur(0, i));
+        }
+        System.out.println(Math.min(memo[0][0], Math.min(memo[0][1], memo[0][2])));
+
+
     }
 
-    public static int recur(int cnt, int pre){
+    public static int recur(int cnt, int color){
         if(cnt == n) return 0;
-        int ret = 1000000;
+        if(memo[cnt][color] != 1000000) return memo[cnt][color];
+
         for (int i = 0; i < 3; i++) {
-            if(i == pre) continue;
-            if(dp[cnt][i] == 0) dp[cnt][i] = recur(cnt + 1, i) + map[cnt][i];
-            ret = Math.min(ret, dp[cnt][i]);
+            if(color == i) continue;
+
+            memo[cnt][color] = Math.min(memo[cnt][color], recur(cnt + 1, i) + map[cnt][i]);
         }
-        return ret;
+        return memo[cnt][color];
     }
+
     public static void input() throws Exception{
-        strToken();
-        n = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(br.readLine());
+
         map = new int[n][3];
-        dp = new int[n][3];
-        ans = Integer.MAX_VALUE;
+        memo = new int[n + 1][3];
         for (int i = 0; i < n; i++) {
-            strToken();
-            for (int j = 0; j < 3; j++) {
+            st = new StringTokenizer(br.readLine());
+            Arrays.fill(memo[i], 1000000);
+
+            for(int j = 0; j < 3; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-    }
-    public static void strToken() throws Exception{
-        st = new StringTokenizer(br.readLine());
     }
 }
