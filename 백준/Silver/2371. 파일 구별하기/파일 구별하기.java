6c1fc3969b8e2c2654abe98d61c2021d031ca763
Leechaelim <chaelim3396@gmail.com>
2024-10-28 20:23:50 +0900

    [Silver III] Title: 파일 구별하기, Time: 216 ms, Memory: 19568 KB -BaekjoonHub

--- 백준/Silver/2371. 파일 구별하기/파일 구별하기.java
+++ 백준/Silver/2371. 파일 구별하기/파일 구별하기.java
@@ -2,15 +2,15 @@ import java.util.*;
 import java.io.*;
 public class Main {
     static final String EMPTY = "0 ";
-    static int n, k;
+    static int n, k, ans;
     static ArrayList<Integer>[] files;
+    static Map<Integer, String>[] test;
     static StringTokenizer st;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static void main(String[] args) throws Exception {
         pre_setting();
-        int ans = -1;
-//        int ans = binary_search();
+
         for(int i = 1; i < k; i++){
             if(is_divided(i)) {
                 ans = i;
@@ -21,61 +21,46 @@ public class Main {
         System.out.println(ans);;
     }
 
-//    static int binary_search(){
-//        int s, e, m, ans;
-//
-//        s = 0;
-//        e = k;
-//        ans = -1;
-//
-//        while(s < e){
-//            m = (s + e) / 2;
-//            System.out.println(m);
-//            if(is_divided(m)){
-//                ans = m;
-//                s = m;
-//            }else{
-//                ans = m;
-//                e = m;
-//            }
-//        }
-//        return ans;
-//    }
-
     static boolean is_divided(int standard){
         Map<String, Integer> map = new HashMap<>();
 
         String now;
+        int empty = 0;
         for(int i = 0; i < n; i++){
-            now = "";
-
-            if(files[i].size() < standard) {
-                for (int j = 0; j < files[i].size(); j++) now += files[i].get(j) + " ";
-//                for(int j = files[i].size(); j < standard; j++) now += EMPTY;
-
+            if(test[i].containsKey(standard)) {
+                map.put(test[i].get(standard), 1);
             }else{
-                for(int j = 0; j < standard; j++) now += files[i].get(j) + " ";
+                empty++;
             }
-
-            map.put(now, 1);
         }
-        if(map.size() == n) return true;
+        if(map.size() + empty == n) return true;
         return false;
     }
 
     static void pre_setting() throws Exception{
         n = Integer.parseInt(br.readLine());
         files = new ArrayList[n];
+        test = new HashMap[n];
 
-        int now;
+        int now, idx;
+        String str, cur;
         for(int i = 0; i < n; i++){
             files[i] = new ArrayList<>();
+            test[i] = new HashMap<>();
             st = new StringTokenizer(br.readLine());
             k = Math.max(k, st.countTokens());
 
+            str = "";
+            idx = 1;
             while(st.hasMoreTokens()){
-                now = Integer.parseInt(st.nextToken());
-                if(now != - 1) files[i].add(now);
+
+                cur = st.nextToken();
+                str += cur + " ";
+                now = Integer.parseInt(cur);
+                if(now != - 1) {
+                    files[i].add(now);
+                    test[i].put(idx++, str);
+                }
             }
         }
     }
