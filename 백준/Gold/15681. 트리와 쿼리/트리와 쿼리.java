aba4b3ca0e143bfc8ae555eaa35c449cc9a73218
Leechaelim <chaelim3396@gmail.com>
2024-11-07 18:09:31 +0900

    [Gold V] Title: 트리와 쿼리, Time: 548 ms, Memory: 81740 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
@@ -2,27 +2,25 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     static int n, r, q;
-    static int[] sub, query;
-    static List<Integer>[] graph;
+    static int[] sub, output_list;
+    static ArrayList<Integer> graph[];
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static void main(String[] args) throws Exception{
-        input();
+        pre_setting();
         dfs(r, 0);
-        for (int i = 0; i < q; i++) {
-            sb.append(sub[query[i]]).append("\n");
-        }
-        System.out.println(sb);
+        for(int i = 0; i < q; i++) sb.append(sub[output_list[i]]).append("\n");
+        bw.append(sb);
+        bw.close();
     }
 
-    public static void dfs(int now, int pre){
-
+    static void dfs(int now, int pre){
         int next;
-        for (int i = 0; i < graph[now].size(); i++) {
+
+        for(int i = 0; i < graph[now].size(); i++){
             next = graph[now].get(i);
 
             if(next == pre) continue;
@@ -31,34 +29,29 @@ public class Main {
         }
     }
 
-    public static void input() throws Exception{
+    static void pre_setting() throws Exception{
         st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         r = Integer.parseInt(st.nextToken());
         q = Integer.parseInt(st.nextToken());
 
+        graph = new ArrayList[n + 1];
         sub = new int[n + 1];
-        query = new int[q];
+        output_list = new int[q];
         Arrays.fill(sub, 1);
+        for(int i = 0; i < n + 1; i++) graph[i] = new ArrayList<>();
 
-        graph = new List[n + 1];
-        for (int i = 0; i < n + 1; i++) {
-            graph[i] = new ArrayList<>();
-        }
-
-        int u, v;
-        for (int i = 0; i < n - 1; i++) {
+        int a, b;
+        for(int i = 0; i < n - 1; i++){
             st = new StringTokenizer(br.readLine());
-            u = Integer.parseInt(st.nextToken());
-            v = Integer.parseInt(st.nextToken());
 
-            graph[u].add(v);
-            graph[v].add(u);
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            graph[a].add(b);
+            graph[b].add(a);
         }
 
-        for (int i = 0; i < q; i++) {
-            query[i] = Integer.parseInt(br.readLine());
-        }
+        for(int i = 0; i < q; i++) output_list[i] = Integer.parseInt(br.readLine());
     }
-
 }
