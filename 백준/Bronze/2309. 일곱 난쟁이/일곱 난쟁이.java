af4083092192f901a44b801adf5e0b48ec4baeb6
Leechaelim <chaelim3396@gmail.com>
2025-01-27 17:01:47 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,50 +1,39 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-
-    static int total;
-    static int[] people, selected;
+    static boolean flag;
+    static int[] cm, selected;
     static StringBuilder sb = new StringBuilder();
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    /*
-        note
-            난쟁이 9명 중 7먕을 골라 100만들기
-            1. 모든 난쟁이의 조함 => 9C7
-            2. 모든 난쟁이 합 - 2명 골라서 빼기
-
-    */
-    public static void main(String[] args) throws Exception{
-        people = new int[9];
-        selected = new int[2];
-
-        for (int i = 0; i < 9; i++) {
-            people[i] = Integer.parseInt(br.readLine());
-            total += people[i];
-        }
-        Arrays.sort(people);
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws Exception {
+        pre_setting();
         combi(0, 0, 0);
+        bw.append(sb);
+        bw.close();
+
     }
 
-    public static void combi(int start, int cnt, int sum){
-        if(cnt == 2){
-            if(total - sum == 100){
-                for (int i = 0; i < 9; i++) {
-
-                    if(selected[0] == people[i]) continue;
-                    if(selected[1] == people[i]) continue;
-                    sb.append(people[i]).append("\n");
-
-                }
-                System.out.println(sb);
-                System.exit(0);
+    static void combi(int cnt, int sum, int start){
+        if(cnt == 7){
+            if(sum == 100 && !flag){
+                for(int i = 0; i < 7; i++) sb.append(selected[i]).append("\n");
+                flag = true;
             }
             return;
         }
-        for (int i = start; i < 9; i++) {
-            selected[cnt] = people[i];
-            combi(i + 1, cnt + 1, sum + people[i]);
+        for(int i = start; i < 9; i++){
+            selected[cnt] = cm[i];
+            combi(cnt + 1, sum + cm[i], i + 1);
         }
+    }
+
+    static void pre_setting() throws Exception{
+        cm = new int[9];
+        selected = new int[7];
+
+        for(int i = 0; i < 9; i++) cm[i] = Integer.parseInt(br.readLine());
+        Arrays.sort(cm);
     }
 
 }
