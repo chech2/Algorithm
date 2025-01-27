380648043724826fef8e9ef9e3dcca1356d5d1fc
Leechaelim <chaelim3396@gmail.com>
2025-01-27 16:45:36 +0900

    [Bronze III] Title: 통학의 신, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/17945. 통학의 신/통학의 신.java
+++ 백준/Bronze/17945. 통학의 신/통학의 신.java
@@ -1,22 +1,36 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static int a, b;
+    static StringBuilder sb;
     static StringTokenizer st;
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder sb = new StringBuilder();
-    
+    static BufferedReader br;
+    static BufferedWriter bw;
     public static void main(String[] args) throws Exception{
-        st = new StringTokenizer(br.readLine());
-        a = Integer.parseInt(st.nextToken());
-        b = Integer.parseInt(st.nextToken());
-
-        for (int i = -1000; i < 1001; i++) {
-            if(i*i + 2*i*a + b == 0) sb.append(i).append(" ");
-        }
-        System.out.println(sb);
+        pre_setting();
+        find_answer();
+        bw.append(sb);
+        bw.close();
     }
 
+    static void find_answer(){
+        int now;
+
+        for(int i = -1000; i < 1001; i++){
+            now = i * i + a * i + b;
+
+            if (now == 0) sb.append(i).append(" ");
+        }
+    }
+
+    static void pre_setting() throws Exception{
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
+        a = Integer.parseInt(st.nextToken()) * 2;
+        b = Integer.parseInt(st.nextToken());
+    }
 }
