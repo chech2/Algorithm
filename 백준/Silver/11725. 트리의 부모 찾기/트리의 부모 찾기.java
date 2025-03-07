ff63cb015a548d862cb834e2f92756efb56ac9e1
Leechaelim <chaelim3396@gmail.com>
2025-03-08 08:07:54 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 484 ms, Memory: 61576 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,51 +1,57 @@
 import java.io.*;
 import java.util.*;
-public class Main {
 
+public class Main {
     static int n;
     static int[] p;
-    static List<Integer>[] graph;
+    static List<Integer>[] tree;
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
+
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static void main(String[] args) throws Exception{
-        input();
-        dfs(1, 0);
-        for (int i = 2; i < n + 1; i++) {
-            sb.append(p[i]).append("\n");
-        }
-        System.out.println(sb);
+        preSetting();
+        dfs(0, 1);
+        print();
+        
+    }
+    
+    static void print() throws Exception{
+        for(int i = 2; i < n + 1; i++) sb.append(p[i]).append("\n");
+        bw.append(sb);
+        bw.close();
     }
 
-    public static void dfs(int now, int pre){
+    static void dfs(int pre, int now){
 
         int next;
-        for (int i = 0; i < graph[now].size(); i++) {
-            next = graph[now].get(i);
+        for(int i = 0; i < tree[now].size(); i++){
+            next = tree[now].get(i);
+
             if(next == pre) continue;
-
             p[next] = now;
-            dfs(next, now);
+            dfs(now, next);
         }
     }
 
-    public static void input () throws Exception{
+    static void preSetting() throws Exception{
         n = Integer.parseInt(br.readLine());
+
         p = new int[n + 1];
-        graph = new ArrayList[n + 1];
-        for (int i = 0; i < n + 1; i++) {
-            graph[i] = new ArrayList<>();
-        }
+        tree = new ArrayList[n + 1];
 
-        int u, v;
-        for (int i = 0; i < n - 1; i++) {
+        p[1] = 1;
+        for(int i = 0; i < n + 1; i++) tree[i] = new ArrayList<>();
+
+        int a, b;
+        for(int i = 0 ; i < n - 1; i++) {
             st = new StringTokenizer(br.readLine());
-            u = Integer.parseInt(st.nextToken());
-            v = Integer.parseInt(st.nextToken());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
 
-            graph[u].add(v);
-            graph[v].add(u);
+            tree[a].add(b);
+            tree[b].add(a);
         }
     }
-
 }
