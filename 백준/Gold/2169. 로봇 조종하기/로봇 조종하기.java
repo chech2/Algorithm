254d4f77ccec12d8087fe6eddbbd630b96b63f13
Leechaelim <chaelim3396@gmail.com>
2025-03-24 17:06:18 +0900

    [Gold II] Title: 로봇 조종하기, Time: 608 ms, Memory: 85588 KB -BaekjoonHub

--- 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.java
+++ 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.java
@@ -2,55 +2,63 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static final int MIN = -1000000;
-    static int n, m, ans;
+    final static int MIN = -100000000;
+    static int[] dx = {0, 1, 0};
+    static int[] dy = {1, 0, -1};
+    static int n, m;
     static int[][] map;
     static int[][][] dp;
-    static int[] dx = {1, 0, 0};
-    static int[] dy = {0, -1, 1};
     static StringTokenizer st;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
     public static void main(String[] args) throws Exception{
-        input();
-        System.out.println(recur(0, 0,  0) + map[0][0]);
+        preSetting();
+        
+        for(int i = 0; i < 3; i++) {
+            dp[i][0][0] = recur(i, 0, 0) + map[0][0];
+        }
+        System.out.println(Math.max(Math.max(dp[0][0][0], dp[1][0][0]), dp[2][0][0]));
     }
 
-    static void input() throws Exception{
-        st = new StringTokenizer(br.readLine());
+    static int recur(int d, int x, int y){
+        if(x == n - 1 && y == m - 1) return 0;
+        if(dp[d][x][y] != -1) return dp[d][x][y];
 
+        int nx, ny;
+        int rot = MIN;
+        for(int i = 0; i < 3; i++){
+            nx = x + dx[i];
+            ny = y + dy[i];
+
+            if(isImpossible(d, i, nx, ny)) continue;
+            rot = Math.max(rot, recur(i, nx, ny) + map[nx][ny]);
+        }
+        return dp[d][x][y] = rot;
+    }
+
+    static boolean isImpossible(int d, int nd, int nx, int ny){
+        if(nx < 0 || ny < 0 || n <= nx || m <= ny) return true;
+        if(d == 0 && nd == 2 || d == 2 && nd == 0) return true;
+        return false;
+    }
+
+    static void preSetting() throws Exception{
+
+        st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
         map = new int[n][m];
-        dp = new int[n][m][3];
-        for (int i = 0; i < n; i++) {
+        dp = new int[3][n][m];
+        for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
 
-            for(int j = 0; j < m; j++) {
+            for(int j = 0; j < m; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
-                Arrays.fill(dp[i][j], MIN);
             }
         }
-    }
 
-    static int recur(int d, int r, int c){
-        if(r == n - 1 && c == m - 1) return 0;
-
-        if(dp[r][c][d] != MIN) return dp[r][c][d];
-        int nx, ny;
-        int rot = MIN;
         for(int i = 0; i < 3; i++){
-            nx = r + dx[i];
-            ny = c + dy[i];
-
-            if(nx < 0 || n <= nx || ny < 0 || m <= ny) continue;
-            if((d == 1 && i == 2) || (d == 2 && i == 1)) continue;
-
-            rot = Math.max(rot, recur(i, nx, ny) + map[nx][ny]);
+            for(int j = 0; j < n; j++) Arrays.fill(dp[i][j], -1);
         }
-
-        return dp[r][c][d] = rot;
     }
 }
