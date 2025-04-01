d31ed8c9c726540db64de0abc16579cf97f0dcec
Leechaelim <chaelim3396@gmail.com>
2025-04-01 16:07:37 +0900

    [level 2] Title: 완전범죄, Time: 2.82 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/README.md
+++ 프로그래머스/2/389480. 완전범죄/README.md
@@ -1,10 +1,10 @@
 # [level 2] 완전범죄 - 389480 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389480#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389480) 
 
 ### 성능 요약
 
-메모리: 86.4 MB, 시간: 0.56 ms
+메모리: 81 MB, 시간: 2.82 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 04일 20:35:07
+2025년 04월 01일 16:07:34
 
 ### 문제 설명
 
--- 프로그래머스/2/389480. 완전범죄/완전범죄.java
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -1,44 +1,36 @@
 import java.util.*;
 
-class Solution {    
-    int INF = 50000;
+class Solution {
+    int n, m, size;
+    int[][] info;
+    int[][][] dp;
+    
     public int solution(int[][] info, int n, int m) {
-        int answer = dp(info, n, m);
+        preSetting(info, n, m);
+        int answer = recur(0, 0, 0);
         
-        if(answer == INF) return -1;
+        if(500 <= answer) answer = -1;
         return answer;
     }
     
-    int dp(int[][] info, int n, int m){
-        int size = info.length;
-        int[][] memo = new int[size + 1][m];
-        for(int i = 1; i < size + 1; i++) Arrays.fill(memo[i], INF);
+    int recur(int cnt, int a, int b){
+        if(n <= a || m <= b) return 500;
+        if(cnt == size) return 0;
+        if(dp[cnt][a][b] != -1) return dp[cnt][a][b];
         
-        int a, b;
-        for(int i = 1; i < size + 1; i++){
-            a = info[i - 1][0];
-            b = info[i - 1][1];
+        dp[cnt][a][b] = recur(cnt + 1, a + info[cnt][0], b) + info[cnt][0];
+        return dp[cnt][a][b] = Math.min(dp[cnt][a][b], recur(cnt + 1, a, b + info[cnt][1]));
+    }
+        
+    void preSetting(int[][] info, int n, int m){
+        this.info = info;
+        this.n = n;
+        this.m = m;
+        this.size = info.length;
+        dp = new int[size][n][m];
             
-            // System.out.println("지금: " + i);
-            for(int j = 0; j < m; j++){
-                memo[i][j] = Math.min(memo[i][j], memo[i - 1][j] + a);
-                if(j + b < m) {
-                    memo[i][j + b] = Math.min(memo[i - 1][j], memo[i][j + b]);
-                    // System.out.println(memo[i][j + b]);
-                }
-                // System.out.println(memo[i][j]);
-            }
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < n; j++) Arrays.fill(dp[i][j], -1);
         }
-        // for(int i = 0; i < size + 1; i++) System.out.println(Arrays.toString(memo[i]));
-
-        
-        
-        
-        int ans = INF;
-        for(int i = 0; i < m; i++) {
-            if(memo[size][i] < n) ans = Math.min(ans, memo[size][i]);
-        }
-        // System.out.println(":: " + ans);
-        return ans;
     }
 }
