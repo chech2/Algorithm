31303cac2ff57048310ef12f7907c37ecf2d48bd
Leechaelim <chaelim3396@gmail.com>
2025-07-26 22:25:22 +0900

    [Silver III] Title: 피보나치 함수, Time: 100 ms, Memory: 14036 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,45 +1,56 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int ONE[], Zero[];
-
+    static int t;
+    static int[] num;
+    static int[][] fibonArr;
+    static StringBuilder sb = new StringBuilder();
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int TC =  Integer.parseInt(br.readLine());
-        int n;
+        inputSetting();
 
-        for (int t = 0; t < TC; t++) {
-            n = Integer.parseInt(br.readLine());
-            if(n != 0) {
-                ONE = new int[n + 1];
-                Zero = new int[n + 1];
-
-                ONE[0] = 0;
-                ONE[1] = 1;
-                Zero[0] = 1;
-                Zero[1] = 0;
-
-                ONE[n] = fibonacciONE(n);
-                Zero[n] = fibonacciZero(n);
-                sb.append(Zero[n]).append(" ").append(ONE[n]).append("\n");
-
-            }else sb.append("1 0\n");
+        for(int i = 0; i < 41; i++) {
+            fibonArr[0][i] = fibonacciZero(i);
+            fibonArr[1][i] = fibonacciOne(i);
         }
-        System.out.println(sb);
 
+        for(int i = 0; i < t; i++) sb.append(fibonArr[0][num[i]]).append(" ").append(fibonArr[1][num[i]]).append("\n");
+        bw.append(sb);
+        bw.close();
     }
 
-    static int fibonacciONE(int n){
-        if(n > 1 && ONE[n] == 0)
-            ONE[n] = fibonacciONE(n  - 1) + fibonacciONE(n - 2);
-        return ONE[n];
+    static int fibonacciZero(int now){
+        if(now < 0) return 0;
+        if(fibonArr[0][now] != -1) return fibonArr[0][now];
+
+        if(now == 0) return 1;
+
+        return fibonArr[0][now] = fibonacciZero(now - 1) + fibonacciZero(now - 2);
     }
-    static int fibonacciZero(int n){
-        if(n > 1 && Zero[n] == 0)
-            Zero[n] = fibonacciZero(n  - 1) + fibonacciZero(n - 2);
-        return Zero[n];
+
+    static int fibonacciOne(int now){
+        if(now <= 0) return 0;
+        if(fibonArr[1][now] != -1) return fibonArr[1][now];
+
+        if(now == 1) return 1;
+
+        return fibonArr[1][now] = fibonacciOne(now - 1) + fibonacciOne(now - 2);
+    }
+
+
+    static void inputSetting() throws Exception{
+        t = Integer.parseInt(br.readLine());
+        num = new int[t];
+        fibonArr = new int[2][41];
+
+        for(int i = 0; i < 2; i++) Arrays.fill(fibonArr[i], -1);
+        for(int i = 0; i < t; i++) num[i] = Integer.parseInt(br.readLine());
+
+        fibonArr[0][0] = 1;
+        fibonArr[0][1] = 0;
+        fibonArr[1][0] = 0;
+        fibonArr[1][1] = 1;
     }
 }
