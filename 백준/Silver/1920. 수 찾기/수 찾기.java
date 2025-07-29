a9f9355359a702034d31796b715c9dc583b3684f
Leechaelim <chaelim3396@gmail.com>
2025-07-29 21:56:32 +0900

    [Silver IV] Title: 수 찾기, Time: 556 ms, Memory: 48764 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,35 +2,30 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static Set<Integer> set;
-    static int[] qList;
-    static int N, M;
     static StringTokenizer st;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static void main(String[] args) throws Exception{
-        inputSetting();
-        for(int i = 0; i < M; i++){
-            if(set.contains(qList[i])) bw.append('1');
-            else bw.append('0');
-            
-            bw.append('\n');
-        }
+        int N, M, find;
 
-        bw.close();
-    }
-
-    static void inputSetting() throws Exception{
         N = Integer.parseInt(br.readLine());
-        set = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>();
 
         st = new StringTokenizer (br.readLine());
         for(int i = 0; i < N; i++) set.add(Integer.parseInt(st.nextToken()));
 
         M = Integer.parseInt(br.readLine());
-        qList = new int[M];
-
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < M; i++) qList[i] = Integer.parseInt(st.nextToken());
+
+        for(int i = 0; i < M; i++){
+            find = Integer.parseInt(st.nextToken());
+
+            if(set.contains(find)) bw.append('1');
+            else bw.append('0');
+
+            bw.append('\n');
+        }
+
+        bw.close();
     }
 }
