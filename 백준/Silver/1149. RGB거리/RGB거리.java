30fa65b3f89a648c788a11384186208c6687ed37
Leechaelim <chaelim3396@gmail.com>
2025-08-03 19:26:18 +0900

    [Silver I] Title: RGB거리, Time: 108 ms, Memory: 14696 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,43 +1,42 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-    static int n;
-    static int[][] map, dp;
-    static final int max = 1000000;
+    static int[][] house, dp;
+    static int n, ans;
     static StringTokenizer st;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static void main(String[] args) throws Exception{
-        input();
-        System.out.println(recur(0, 0));
+        inputSetting();
+
+        System.out.println(subSet(0, 0));
     }
 
-    static int recur(int cnt, int pre){
-        if(cnt == n){
-            return 0;
+    static int subSet(int cnt, int pre){
+        if(cnt == n) return 0;
+        if(dp[cnt][pre] != Integer.MAX_VALUE) return dp[cnt][pre];
+
+        int rot = Integer.MAX_VALUE;
+        for(int i = 1; i < 4; i++){
+            if(pre == i) continue;
+
+            dp[cnt][pre] = Math.min(subSet(cnt + 1, i) + house[cnt][i], dp[cnt][pre]);
         }
-        if(dp[cnt][pre] != 1000) return dp[cnt][pre];
-        int rot = max;
-        for(int i = 1; i < 4; i++) {
-            if(i == pre) continue;
-            rot = Math.min(rot, recur(cnt + 1, i) + map[cnt][i]);
-        }
-        return dp[cnt][pre] = rot;
+        return dp[cnt][pre];
     }
 
-    static void input() throws Exception{
+    static void inputSetting() throws Exception{
         n = Integer.parseInt(br.readLine());
+        house = new int[n][4];
+        dp = new int[n + 1][4];
+        ans = Integer.MAX_VALUE;
 
-        map = new int[n][4];
-        dp = new int[n][4];
         for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
 
-            Arrays.fill(dp[i], 1000);
-            for(int j = 1; j < 4; j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
+            for(int j = 1; j < 4; j++) house[i][j] = Integer.parseInt(st.nextToken());
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
         }
-
+        Arrays.fill(dp[n], Integer.MAX_VALUE);
     }
 }
