b7005d5e1eb12c0754afde16f160092da7a582a5
Leechaelim <chaelim3396@gmail.com>
2025-08-17 18:50:08 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,39 +1,43 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-    static boolean flag;
-    static int[] cm, selected;
+
+    static int[] numbers, selected, answer;
     static StringBuilder sb = new StringBuilder();
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
     public static void main(String[] args) throws Exception {
-        pre_setting();
+        numbers = new int[9];
+        selected = new int[7];
+        answer = new int[7];
+
+        for (int i = 0; i < 9; i++) {
+            numbers[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(numbers);
         combi(0, 0, 0);
+        
+        for (int i = 0; i < 7; i++) sb.append(answer[i]).append("\n");
         bw.append(sb);
         bw.close();
-
     }
 
-    static void combi(int cnt, int sum, int start){
-        if(cnt == 7){
-            if(sum == 100 && !flag){
-                for(int i = 0; i < 7; i++) sb.append(selected[i]).append("\n");
-                flag = true;
+    static void combi(int cnt, int start, int sum) {
+        if (cnt == 7) {
+            if (sum == 100) {
+                for (int i = 0; i < 7; i++) {
+                    answer[i] = selected[i];
+                }
             }
             return;
         }
-        for(int i = start; i < 9; i++){
-            selected[cnt] = cm[i];
-            combi(cnt + 1, sum + cm[i], i + 1);
+
+        for (int i = start; i < 9; i++) {
+            selected[cnt] = numbers[i];
+            combi(cnt + 1, i + 1, sum + numbers[i]);
         }
     }
-
-    static void pre_setting() throws Exception{
-        cm = new int[9];
-        selected = new int[7];
-
-        for(int i = 0; i < 9; i++) cm[i] = Integer.parseInt(br.readLine());
-        Arrays.sort(cm);
-    }
-
 }
