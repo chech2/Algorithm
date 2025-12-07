d9a3f3fda6cfc7f008bff5ddfd15451fcb056aa5
Leechaelim <chaelim3396@gmail.com>
2025-12-08 01:23:09 +0900

    [Silver III] Title: 수열, Time: 248 ms, Memory: 23472 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,35 +1,25 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int n, k;
-    static int[] s;
-    static StringTokenizer st;
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) throws Exception{
-        input();
-        System.out.println(mintemp());
-    }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static int mintemp(){
-        int ans = -20000000;
-        for (int i = n; -1 < i - k; i--) {
-            ans = Math.max(ans, s[i] - s[i - k]);
-        }
-        return ans;
-    }
+        int n, m, ans;
+        int[] sum;
 
-    public static void input() throws Exception{
-        st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        s = new int[n + 1];
+        m = Integer.parseInt(st.nextToken());
+        ans = -10000000;
+        sum = new int[n + 1];
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i < n + 1; i++){
-            s[i] = s[i - 1] + Integer.parseInt(st.nextToken());
+        for(int i = 1; i < n + 1; i++) sum[i] = sum[i - 1] + Integer.parseInt(st.nextToken());
+        for(int i = m; i < n + 1; i++){
+            ans = Math.max(ans, sum[i] - sum[i - m]);
         }
+        System.out.println(ans);
     }
 }
