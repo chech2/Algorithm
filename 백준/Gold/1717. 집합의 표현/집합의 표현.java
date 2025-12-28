83c22c261ec0147abeaf4a9746bcacf6e68d7e26
Leechaelim <chaelim3396@gmail.com>
2025-12-28 12:00:00 +0900

    [Gold V] Title: 집합의 표현, Time: 380 ms, Memory: 52316 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,54 +1,59 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int parent[];
-	static int N;
-	static int M;
-	
-	
-	private static int getparent(int x) {
-		if(parent[x] == x) return x;
-		return getparent(parent[x]);
-	}
-	
-	
-	private static void unionparent(int a, int b) {
-		a = getparent(a);
-		b = getparent(b);
-		if(a < b) parent[b] = a;
-		else parent[b] = getparent(a);
-	}
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		parent = new int [N + 1];
-		for (int i = 0; i < N + 1; i++) parent[i] = i;
-		
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int calculation = Integer.parseInt(st.nextToken());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
+    static final String Y = "YES\n";
+    static final String N = "NO\n";
+    static int n, m;
+    static int[] p;
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws Exception{
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        p = new int[n + 1];
 
-			 //부모 합치기 
-			if(calculation == 0) {
-				unionparent(a, b);
-				
-			}else {
-				if(getparent(a) == getparent(b)) sb.append("YES").append("\n");
-				else sb.append("NO").append("\n");
-			}
-		}
-		System.out.println(sb);
-		
-	
-	}
+        for(int i = 0; i < n + 1; i++) p[i] = i;
 
+        int cal, a, b;
+        for(int i = 0; i < m; i++){
+            st = new StringTokenizer(br.readLine());
+            cal = Integer.parseInt(st.nextToken());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+
+            if(cal == 0){
+                if(a == b) continue;
+                union(a, b);
+            }else{
+                if(find(a) == find(b)) sb.append(Y);
+                else sb.append(N);
+            }
+        }
+        bw.append(sb);
+        bw.close();
+    }
+
+    static int find(int a){
+        if(p[a] == a) return p[a];
+        return find(p[a]);
+    }
+
+    static boolean union(int a, int b){
+        a = find(a);
+        b = find(b);
+
+        if(a == b){
+            return true;
+        } else if(a < b){
+            p[a] = b;
+        } else{
+            p[b] = a;
+        }
+        return false;
+    }
 }
+
