ba1ed88fd45c0a61630c6255ba7be6ca95cd3a49
Leechaelim <chaelim3396@gmail.com>
2026-02-02 02:22:15 +0900

    [Gold IV] Title: 비슷한 단어, Time: 1232 ms, Memory: 17716 KB -BaekjoonHub

--- 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
@@ -30,9 +30,8 @@ public class Main {
     }
 
     private static int compareStrs(String s1, String s2){
-        if(s1.equals(s2)) return 0;
-
         int answer = 0;
+        
         for(int i = 0; i < Math.min(s1.length(), s2.length()); i++){
             if(s1.charAt(i) != s2.charAt(i)) break;
 
