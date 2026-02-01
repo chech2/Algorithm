7b39c59e4f51e541d6f6fe782d559e8009bfaa8a
Leechaelim <chaelim3396@gmail.com>
2026-02-02 01:51:50 +0900

    [Silver II] Title: 연속합, Time: 232 ms, Memory: 23724 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,26 +1,29 @@
-import java.io.*;
-import java.nio.Buffer;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Main {
-    static int n, ans;
-    static int[] s;
-    static StringTokenizer st;
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static int n, result;
+    static int[] arr;
     public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        s = new int[n + 1];
 
-        int now;
-        ans = Integer.MIN_VALUE;
-        st = new StringTokenizer(br.readLine());
+        arr = new int[n + 1];
+        Arrays.fill(arr, -10001);
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        result = -10001;
+        int num;
         for (int i = 1; i < n + 1; i++) {
-            now = Integer.parseInt(st.nextToken());
+            num = Integer.parseInt(st.nextToken());
+            arr[i] = Math.max(num, arr[i - 1] + num);
 
-            // 지금까지 값 중 최대 값 저장
-            s[i] = Math.max(s[i - 1] + now, now);
-            ans = Math.max(ans, s[i]);
+            result = Math.max(arr[i], result);
         }
-        System.out.println(ans);
+        System.out.println(result);
     }
 
 }
