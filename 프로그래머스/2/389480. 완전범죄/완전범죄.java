7432761bab91d50d9cea854eeea954acb503dfad
Leechaelim <chaelim3396@gmail.com>
2026-03-04 22:43:23 +0900

    [level 2] Title: 완전범죄, Time: 2.77 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.java
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -1,36 +1,37 @@
 import java.util.*;
 
 class Solution {
-    int n, m, size;
-    int[][] info;
     int[][][] dp;
-    
+    int[][] info;
+    int n, m, CNT;
     public int solution(int[][] info, int n, int m) {
-        preSetting(info, n, m);
-        int answer = recur(0, 0, 0);
-        
-        if(500 <= answer) answer = -1;
-        return answer;
-    }
-    
-    int recur(int cnt, int a, int b){
-        if(n <= a || m <= b) return 500;
-        if(cnt == size) return 0;
-        if(dp[cnt][a][b] != -1) return dp[cnt][a][b];
-        
-        dp[cnt][a][b] = recur(cnt + 1, a + info[cnt][0], b) + info[cnt][0];
-        return dp[cnt][a][b] = Math.min(dp[cnt][a][b], recur(cnt + 1, a, b + info[cnt][1]));
-    }
-        
-    void preSetting(int[][] info, int n, int m){
+        CNT = info.length;
         this.info = info;
         this.n = n;
         this.m = m;
-        this.size = info.length;
-        dp = new int[size][n][m];
-            
-        for(int i = 0; i < size; i++) {
-            for(int j = 0; j < n; j++) Arrays.fill(dp[i][j], -1);
+        dp = new int[CNT][n][m];
+        
+        for(int i = 0; i < CNT; i++){
+            for(int j = 0; j < n; j++) {
+                Arrays.fill(dp[i][j], -1);
+            }
         }
+
+        
+        int answer = recur(0, 0, 0);
+        if(answer == 1000) answer = -1;
+        
+        return answer;
+    }
+    
+    private int recur(int cnt, int a, int b){
+        if(cnt == CNT) return 0;
+        if(dp[cnt][a][b] != -1) return dp[cnt][a][b];
+        
+        dp[cnt][a][b] = 1000;
+        if(a + info[cnt][0] < n) dp[cnt][a][b] = Math.min(dp[cnt][a][b], recur(cnt + 1, a + info[cnt][0], b) + info[cnt][0]);
+        if(b + info[cnt][1] < m) dp[cnt][a][b] = Math.min(dp[cnt][a][b], recur(cnt + 1, a, b + info[cnt][1]));
+        
+        return dp[cnt][a][b];
     }
 }
