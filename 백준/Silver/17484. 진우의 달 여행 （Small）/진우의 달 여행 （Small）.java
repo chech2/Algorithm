47cd35fc7b6f4abd5158fa264a85d73e3db74c6e
Leechaelim <chaelim3396@gmail.com>
2026-03-05 21:43:29 +0900

    [Silver III] Title: 진우의 달 여행 (Small), Time: 104 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.java
+++ 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.java
@@ -2,25 +2,25 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int [][] d = {{1, -1}, {1, 0}, {1, 1}};
-    static int [][] map;
+    static int[][] d = {{0, 0}, {1, -1}, {1, 0}, {1, 1}};
+    static int[][] map;
+    static int[][][] dp;
     static int m, n, ans;
     public static void main(String[] args) throws Exception{
         preSetting();
+
         for(int i = 0; i < m; i++ ){
-            recur(-1, 0, i, map[0][i]);
+            ans = Math.min(ans, recur(0, 0, i) + map[0][i]);
         }
         System.out.println(ans);
     }
 
-    private static void recur(int preD, int x, int y, int oil){
-        if(x == n - 1){
-            ans = Math.min(ans, oil);
-            return;
-        }
-
+    private static int recur(int preD, int x, int y){
+        if(x == n - 1) return 0;
+        if(dp[x][y][preD] != -1) return dp[x][y][preD];
         int nX, nY;
-        for(int i = 0; i < 3; i++){
+        int rot = Integer.MAX_VALUE;
+        for(int i = 1; i < 4; i++){
             if(i == preD) continue;
 
             nX = d[i][0] + x;
@@ -28,8 +28,9 @@ public class Main {
 
             if(n < nX || m <= nY || nY < 0) continue;
 
-            recur(i, nX, nY, oil + map[nX][nY]);
+            rot = Math.min(rot, recur(i, nX, nY) + map[nX][nY]);
         }
+        return dp[x][y][preD] = rot;
     }
 
     private static void preSetting() throws Exception{
@@ -38,6 +39,7 @@ public class Main {
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
+        dp = new int[n][m][4];
 
         map = new int[n][m];
         ans = Integer.MAX_VALUE;
@@ -45,6 +47,7 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < m; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
+                Arrays.fill(dp[i][j], -1);
             }
         }
     }
