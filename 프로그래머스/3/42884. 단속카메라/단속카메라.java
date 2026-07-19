f7b885da0bd9ca850f841339fbe9cc02e30ddc9a
Leechaelim <chaelim3396@gmail.com>
2026-07-20 01:59:20 +0900

    [level 3] Title: 단속카메라, Time: 8.87 ms, Memory: 56.8 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,37 +1,6 @@
-import java.util.*;
-
 class Solution {
-    
     public int solution(int[][] routes) {
         int answer = 0;
-        int jump, r, l, nr, nl;
-        
-        Arrays.sort(routes, (o1, o2) -> {
-            return o2[1] - o1[1];
-        });
-        
-        for(int now = 0; now < routes.length; now++) {
-            
-            jump = 0;
-            l = routes[now][0];
-            r = routes[now][1];
-
-            for(int next = now + 1; next < routes.length; next++){
-                
-                nl = routes[next][0];
-                nr = routes[next][1];
-                
-                if(nr < l) break;
-                if(l <= nl && nr <= r) l = nl;
-                
-                r = nr;
-                jump++;
-            }
-
-            now += jump;
-            answer++;
-        }
-        
         return answer;
-    } 
+    }
 }
